d3c4fcd56a0ac9833cd109633070d08400653880
nguen1225 <Nguen1225@gmail.com>
2020-09-11 19:59:26 +0900

    memoShow作成

--- src/components/memoShow.js
+++ src/components/memoShow.js
@@ -23,4 +23,55 @@ class memoShow extends Component {
 			</div>
 		);
 	}
+
+	onDeleteClick = () => {
+		const { id } = this.props.match.params;
+		this.props.updateMemo(id, values)
+		this.props.history.push('/')
+	}
+
+	render(){
+		const { handleSubmit, pristine, submitting, invalid } = this.props;
+		console.log(submitting)
+		return (
+			<form onSubmit = {handleSubmit(this.onSubmit)}>
+				<div>
+					<Field label = "タイトル" name = "title" type = "text" component = {this.renderField} />
+				</div>
+				<div>
+					<Field label = "メモ" name = "memo" type = "text" component = {this.renderField} />
+				</div>
+				<div>
+					<input type = "submit" value = "更新" disabled = {pristine || submitting || invalid} />
+					<Link to = "/" >キャンセル</Link>
+					<Link to = "/" onClick = {this.onDeleteClick}>削除</Link>
+				</div>
+			</form>
+		);
+	}
 }
+
+const validate = values => {
+	const errors = {};
+	console.log(values);
+	if(!values.title) errors.title = "タイトルを入力してください。";
+	if(!values.memo) errors.memo = "メモを入力してください。";
+
+	return errors;
+}
+
+//Stateの情報をPropsで使用できるようにする。
+const mapStateToProps = (state, ownProps) => {
+	const memo = state.memos[ownProps.match.params.id];
+	//編集画面などで初期化した状態でフォーム画面表示したい場合には、reduxFormのパラメータとしてinitialValuesの名称で初期値を指定する。
+	return { initialValues : memo }
+};
+
+//Dispatch(アクションクリエイター)をPropsで使用できるようにする。
+const mapDispatchToProps = ({ deleteMemo, getMemo, updateMemo});
+
+//コンポーネントをRedux Storeに接続する。
+export default connect(mapStateToProps, mapDispatchToProps)(
+	reduxForm({validate, form : 'memoShowForm', enableReinitialize : true})(memoShow)
+);
+
