8a405a9c2c0b7a102e7431b981f5034c94389728
nguen1225 <Nguen1225@gmail.com>
2020-09-13 23:06:30 +0900

    memoNew.js作成

---
+++ src/components/memoNew.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+
+//Local Import
+import { addMemo } from '../actions;'
