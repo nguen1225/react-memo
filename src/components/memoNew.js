6ff6f7f6f2fadaf195701b68f77f29a9dc820184
nguen1225 <Nguen1225@gmail.com>
2020-09-14 22:38:03 +0900

    memoNew.js作成

--- src/components/memoNew.js
+++ src/components/memoNew.js
@@ -5,3 +5,21 @@ import { Link } from 'react-router-dom';
 
 //Local Import
 import { addMemo } from '../actions;'
+
+//新規作成
+class memoNew extends Component{
+	renderField = (field) => {
+		const { input, label, type, meta : {touched, error} } = field;
+		return (
+			<div>
+				<input {...input} placeholder = {label} type />
+				{touched && error && <span>{error}</span>}
+			</div>
+		);
+	}
+
+	onSubmit = (values) => {
+		this.props.addMemo(values);
+		this.props.history.push('/')
+	}
+}
